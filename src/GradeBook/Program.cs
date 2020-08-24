ad13e77633553e4ee49f3871e9a2c40fbffa8dda
anophoo <ana.gurgenidze11@gmail.com>
2020-08-24 18:25:29 +0400

    add initial files

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> grades = new List<double>();
+            grades.Add(12.1);
+            Console.WriteLine(grades[0]);
+            if (args.Length > 0)
+            {
+                Console.WriteLine($"Hello {args[0]}!");
+            }
+        }
+    }
+}
